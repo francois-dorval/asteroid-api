adef5a5b434667d8df82e52772537efcee828de8
François Dorval <francois.dorval@gmail.com>
2021-01-27 21:42:52 +0100

    commit initial

---
+++ src/main/java/com/github/francoisdorval/asteroidapi/AsteroidApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.francoisdorval.asteroidapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteroidApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsteroidApiApplication.class, args);
+	}
+
+}
